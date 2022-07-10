8134256a24e445fd26aa95478a6fce4767df7199
dopamine-s <motor55005@gmail.com>
2022-07-10 03:37:14 +0300

    Добавляет пропы в промо-фильм  компонента главной страницы

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,12 +1,28 @@
 import Main from '../../pages/main/main';
 
 type AppProps = {
+  filmCardTitle: string;
+  filmCardGenre: string;
+  filmCardYear: number;
   filmCardCount: number;
+  smallFilmCardCount: number;
 }
 
-function App ({filmCardCount}: AppProps): JSX.Element {
+function App (
+  {filmCardCount,
+    smallFilmCardCount,
+    filmCardTitle,
+    filmCardGenre,
+    filmCardYear
+  }: AppProps): JSX.Element {
+
   return (
-    <Main filmCardCount={filmCardCount} />
+    <Main filmCardCount={filmCardCount}
+      smallFilmCardCount={smallFilmCardCount}
+      filmCardTitle={filmCardTitle}
+      filmCardGenre={filmCardGenre }
+      filmCardYear={filmCardYear}
+    />
   );
 }
 
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
-const Setting = {
+const CountSetting = {
   FILM_CARD_COUNT: 9,
+  SMALL_FILM_CARD_COUNT: 20,
+};
+
+const FilmInfo = {
+  FILM_CARD_TITLE: 'The Grand Budapest Hotel',
+  FILM_CARD_GENRE: 'Drama',
+  FILM_CARD_YEAR: 2014,
 };
 
 const root = ReactDOM.createRoot(
@@ -13,7 +20,11 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <App
-      filmCardCount={Setting.FILM_CARD_COUNT}
+      filmCardCount={CountSetting.FILM_CARD_COUNT}
+      smallFilmCardCount={CountSetting.SMALL_FILM_CARD_COUNT}
+      filmCardTitle={FilmInfo.FILM_CARD_TITLE}
+      filmCardGenre={FilmInfo.FILM_CARD_GENRE}
+      filmCardYear={FilmInfo.FILM_CARD_YEAR}
     />
   </React.StrictMode>,
 );

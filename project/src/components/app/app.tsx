e11e8b5be0820fdf18777a4d3a2c997e45f5eb09
dopamine-s <motor55005@gmail.com>
2022-07-14 02:09:38 +0300

    Начинает маршрутизацию. Выделяет лого в компонент

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,12 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AppRoute, AuthorizationStatus } from '../../consts';
 import Main from '../../pages/main/main';
+import SignIn from '../../pages/sign-in/sign-in';
+import MyList from '../../pages/my-list/my-list';
+import Film from '../../pages/film/film';
+import AddReview from '../../pages/add-review/add-review';
+import Player from '../../pages/player/player';
+import NotFound from '../../pages/not-found/not-found';
 
 type AppProps = {
   filmCardTitle: string;
@@ -15,11 +23,57 @@ function App (
   }: AppProps): JSX.Element {
 
   return (
-    <Main filmCardCount={filmCardCount}
-      filmCardTitle={filmCardTitle}
-      filmCardGenre={filmCardGenre }
-      filmCardYear={filmCardYear}
-    />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={
+            <Main
+              filmCardCount={filmCardCount}
+              filmCardTitle={filmCardTitle}
+              filmCardGenre={filmCardGenre}
+              filmCardYear={filmCardYear}
+            />
+          }
+        />
+        <Route
+          path={AppRoute.SignIn}
+          element={
+            <SignIn />
+          }
+        />
+        <Route
+          path={AppRoute.MyList}
+          element={
+            <MyList />
+          }
+        />
+        <Route
+          path={AppRoute.Film}
+          element={
+            <Film />
+          }
+        />
+        <Route
+          path={AppRoute.AddReview}
+          element={
+            <AddReview />
+          }
+        />
+        <Route
+          path={AppRoute.Player}
+          element={
+            <Player />
+          }
+        />
+        <Route
+          path='*'
+          element={
+            <NotFound />
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- project/src/pages/film/film.tsx
+++ project/src/pages/film/film.tsx
@@ -1,3 +1,5 @@
+import Logo from '../../components/logo/logo';
+
 export default function Film(): JSX.Element {
   return (
     <>
@@ -10,13 +12,7 @@ export default function Film(): JSX.Element {
           <h1 className="visually-hidden">WTW</h1>
 
           <header className="page-header film-card__head">
-            <div className="logo">
-              <a href="main.html" className="logo__link">
-                <span className="logo__letter logo__letter--1">W</span>
-                <span className="logo__letter logo__letter--2">T</span>
-                <span className="logo__letter logo__letter--3">W</span>
-              </a>
-            </div>
+            <Logo light={false} isNotMain />
 
             <ul className="user-block">
               <li className="user-block__item">
@@ -145,13 +141,7 @@ export default function Film(): JSX.Element {
         </section>
 
         <footer className="page-footer">
-          <div className="logo">
-            <a href="main.html" className="logo__link logo__link--light">
-              <span className="logo__letter logo__letter--1">W</span>
-              <span className="logo__letter logo__letter--2">T</span>
-              <span className="logo__letter logo__letter--3">W</span>
-            </a>
-          </div>
+          <Logo light isNotMain />
 
           <div className="copyright">
             <p>© 2019 What to watch Ltd.</p>

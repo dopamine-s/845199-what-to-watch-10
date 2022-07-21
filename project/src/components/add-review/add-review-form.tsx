68e1feec5b3d9594bf765f7f59c3c4d66bc4a453
dopamine-s <motor55005@gmail.com>
2022-07-21 07:42:15 +0300

    Сохраняет состояние формы

--- project/src/components/add-review/add-review-form.tsx
+++ project/src/components/add-review/add-review-form.tsx
@@ -1,42 +1,62 @@
+import { Fragment, useState } from 'react';
+
+type Event = {
+  target: {
+    value: string;
+  }
+}
+
 export default function AddReviewForm() {
+  const [userRating, setUserRating] = useState(8);
+  const [userReview, setUserReview] = useState('');
+  // eslint-disable-next-line no-console
+  console.log(userReview);
+  // eslint-disable-next-line no-console
+  console.log(userRating);
+
+  const textareaChangeHandle = (evt: Event) => {
+    const value = evt.target.value;
+    setUserReview(value);
+  };
+
   return (
     <form action="#" className="add-review__form">
       <div className="rating">
         <div className="rating__stars">
-          <input className="rating__input" id="star-10" type="radio" name="rating" value="10" />
-          <label className="rating__label" htmlFor="star-10">Rating 10</label>
-
-          <input className="rating__input" id="star-9" type="radio" name="rating" value="9" />
-          <label className="rating__label" htmlFor="star-9">Rating 9</label>
-
-          <input className="rating__input" id="star-8" type="radio" name="rating" value="8" checked />
-          <label className="rating__label" htmlFor="star-8">Rating 8</label>
-
-          <input className="rating__input" id="star-7" type="radio" name="rating" value="7" />
-          <label className="rating__label" htmlFor="star-7">Rating 7</label>
-
-          <input className="rating__input" id="star-6" type="radio" name="rating" value="6" />
-          <label className="rating__label" htmlFor="star-6">Rating 6</label>
-
-          <input className="rating__input" id="star-5" type="radio" name="rating" value="5" />
-          <label className="rating__label" htmlFor="star-5">Rating 5</label>
-
-          <input className="rating__input" id="star-4" type="radio" name="rating" value="4" />
-          <label className="rating__label" htmlFor="star-4">Rating 4</label>
-
-          <input className="rating__input" id="star-3" type="radio" name="rating" value="3" />
-          <label className="rating__label" htmlFor="star-3">Rating 3</label>
-
-          <input className="rating__input" id="star-2" type="radio" name="rating" value="2" />
-          <label className="rating__label" htmlFor="star-2">Rating 2</label>
-
-          <input className="rating__input" id="star-1" type="radio" name="rating" value="1" />
-          <label className="rating__label" htmlFor="star-1">Rating 1</label>
+          {
+            Array.from({ length: 10 }, (item, index) => index + 1)
+              .reverse()
+              .map((number) => (
+                <Fragment key={number}>
+                  <input
+                    className="rating__input"
+                    id={`star-${number}`}
+                    type="radio" name="rating"
+                    value={number}
+                    onChange={(evt) => setUserRating(Number(evt.target.value))}
+                    defaultChecked={number === userRating}
+                  />
+                  <label
+                    className="rating__label"
+                    htmlFor={`star-${number}`}
+                  >
+                    Rating {number}
+                  </label>
+                </Fragment>
+              ))
+          }
         </div>
       </div>
 
       <div className="add-review__text">
-        <textarea className="add-review__textarea" name="review-text" id="review-text" placeholder="Review text"></textarea>
+        <textarea
+          onChange={textareaChangeHandle}
+          value={userReview}
+          className="add-review__textarea"
+          name="review-text"
+          id="review-text"
+          placeholder="Review text"
+        />
         <div className="add-review__submit">
           <button className="add-review__btn" type="submit">Post</button>
         </div>

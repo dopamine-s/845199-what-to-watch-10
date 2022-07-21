4650a6ea093aecfdd28e025490bb6a9da1762d8a
dopamine-s <motor55005@gmail.com>
2022-07-21 18:34:36 +0300

    Рефакторинг

--- project/src/components/film-card/film-card.tsx
+++ project/src/components/film-card/film-card.tsx
@@ -4,18 +4,19 @@ import { Film } from '../../types/films';
 type FilmCardProps = {
   film: Film;
   // activeFilm: number | null;
-  handleFilmCardMouseOver: (id: number) => void;
-  handleFilmCardMouseOut: () => void;
+  filmCardMouseOverHandler: (id: number) => void;
+  filmCardMouseOutHandler: () => void;
 }
 
-export default function FilmCard({ film, handleFilmCardMouseOver, handleFilmCardMouseOut }: FilmCardProps): JSX.Element {
+export default function FilmCard({ film, filmCardMouseOverHandler, filmCardMouseOutHandler }: FilmCardProps): JSX.Element {
   const { id, name, previewImage } = film;
   const navigate = useNavigate();
+  const mouseOverHandler = (): void => filmCardMouseOverHandler(id);
 
   return (
     <article className="small-film-card catalog__films-card" style={{ cursor: 'pointer'}} onClick={() => navigate(`/films/${film.id}`)}
-      onMouseOver={() => handleFilmCardMouseOver(id)}
-      onMouseOut={handleFilmCardMouseOut}
+      onMouseOver={mouseOverHandler}
+      onMouseOut={filmCardMouseOutHandler}
     >
       <div className="small-film-card__image">
         <img src={previewImage} alt={name} width="280" height="175" />
--- project/src/components/films-list/films-list.tsx
+++ project/src/components/films-list/films-list.tsx
@@ -9,13 +9,11 @@ type FilmsListProps = {
 export default function FilmsList({ films }: FilmsListProps): JSX.Element {
   const [activeFilm, setActiveFilm] = useState <number | null> (null);
 
-  const handleFilmCardMouseOver = (filmId: number): void =>
-    setActiveFilm(filmId);
-    // eslint-disable-next-line no-console
+  const filmCardMouseOverHandler = (filmId: number): void => setActiveFilm(filmId);
+  // eslint-disable-next-line no-console
   console.log(activeFilm);
 
-  const handleFilmCardMouseOut = (): void =>
-    setActiveFilm(null);
+  const filmCardMouseOutHandler = (): void => setActiveFilm(null);
 
   const filmCards = films.map((film) =>
     (
@@ -23,8 +21,8 @@ export default function FilmsList({ films }: FilmsListProps): JSX.Element {
         key={film.id}
         film={film}
         // activeFilm={activeFilm}
-        handleFilmCardMouseOver={handleFilmCardMouseOver}
-        handleFilmCardMouseOut={handleFilmCardMouseOut}
+        filmCardMouseOverHandler={filmCardMouseOverHandler}
+        filmCardMouseOutHandler={filmCardMouseOutHandler}
       />
     ));
 
